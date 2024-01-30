989fb7a120ed7e44cf73b4ba39c9f61099b9e324
Cooked Nick <9829045+cookednick@users.noreply.github.com>
2024-01-30 14:02:19 -0800

    Converted tabs to spaces

--- AwaitReturn.swift
+++ AwaitReturn.swift
@@ -1,19 +1,19 @@
 /// Returns a value from an asynchronous context into a concurrent one.
 public func awaitReturn<V>(_ produceValue: @escaping () async -> V) -> V {
-	let container = AsyncContainer<V>()
-	
-	Task {
-		container.result = await produceValue()
-		container.done = true
-	}
-	
-	while !container.done { }
-	
-	return container.result
+    let container = AsyncContainer<V>()
+    
+    Task {
+        container.result = await produceValue()
+        container.done = true
+    }
+    
+    while !container.done { }
+    
+    return container.result
 }
 
 
 fileprivate final class AsyncContainer<V> {
-	var done = false
-	var result: V!
+    var done = false
+    var result: V!
 }
